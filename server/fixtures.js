6c1e9e874428e4e359e08839dbf415f41954c9cd
Sverre Johann Bjørke <sverrejb@gmail.com>
2016-03-15 15:03:36 +0100

    added loading spinner

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,22 +1,25 @@
 if (Verses.find().count() === 0) {
     Verses.insert({
         title: 'Vers 1',
-        text: 'jeg møtte på en homofil' +
-        'han ville ta meg bakfra' +
-        'men jeg tok å lurte han' +
+        text: 'jeg møtte på en homofil\n ' +
+        'han ville ta meg bakfra\n ' +
+        'men jeg tok å lurte han\n ' +
         'jeg snudde meg og gapa'
     });
 
     Verses.insert({
         title: 'Vers 2',
-        text: 'jeg møtte på en lommemann' +
-        'han hadde no i lomma ' +
-        'det var ikke sukkertøy ' +
+        text: 'jeg møtte på en lommemann\n ' +
+        'han hadde no i lomma\n ' +
+        'det var ikke sukkertøy\n ' +
         'jeg klemte til, det skumma'
     });
 
     Verses.insert({
         title: 'Vers 3',
-        text: 'jeg ble nå med ei kjerring hjem hun sa hun hadde mensen det burde ikke vært så galt men flomma jo som Themsen'
+        text: 'jeg ble nå med ei kjerring hjem\n ' +
+        'hun sa hun hadde mensen\n ' +
+        'det burde ikke vært så galt\n ' +
+        'men flomma jo som Themsen'
     });
 }
