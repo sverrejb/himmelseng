3cc6b5b157a1293fae1eb679e46d7f0577ca1aa4
Sverre Johann Bjørke <sverrejb@gmail.com>
2016-03-22 03:24:15 +0100

    added animation to menu

--- client/templates/application/header.js
+++ client/templates/application/header.js
@@ -2,4 +2,7 @@ Template.header.onRendered(function(){
     $(".button-collapse").sideNav({
         closeOnClick: true
     });
+    $(".button-collapse").click(function(){
+            Materialize.showStaggeredList('#mobile-menu');
+        });
 });
