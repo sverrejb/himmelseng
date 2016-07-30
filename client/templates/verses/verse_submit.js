29d1c4002f7946e294e20976af0557c81c45169e
Nils Herde <mail@hernil.com>
2016-07-30 12:32:34 +0200

    Added verse line number validation to submit form

--- client/templates/verses/verse_submit.js
+++ client/templates/verses/verse_submit.js
@@ -3,16 +3,21 @@ Template.verseSubmit.events({
         event.preventDefault();
         
         const target = event.target;
-        
         const post = {
             title: target.title.value,
             text: target.text.value
         };
 
-        post._id = Verses.insert(post);
-        target.title.value = '';
-        target.text.value = '';
-        Materialize.toast('Takk for ditt bidrag!', 3000);
+        const lineBreakCount = (post.text.match(/\n/g) || []).length;
+        if (lineBreakCount !== 3) {
+            Materialize.toast('husk fire linjer! ', 4000)
+        }
+        else {
+            post._id = Verses.insert(post);
+            target.title.value = '';
+            target.text.value = '';
+            Materialize.toast('Takk for ditt bidrag!', 3000);
+        }
         
     }
 });
