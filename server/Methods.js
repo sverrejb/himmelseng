6148fade3ca0579fd3c3a828a31b25c160e0e081
Sverre Johann Bjørke <sverrejb@gmail.com>
2016-09-08 15:32:47 +0200

    reads target adresses from env variable

---
+++ server/Methods.js
@@ -0,0 +1,20 @@
+/**
+ * Created by sverre on 08/09/16.
+ */
+
+function getEnvEmails() {
+    var addresses = process.env.ADMIN_EMAILS;
+    return addresses.split(",");
+}
+
+Meteor.methods({
+    sendEmail: function (post) {
+        this.unblock();
+        Email.send({
+            to: getEnvEmails(),
+            from: "admin@himmelseng.no",
+            subject: "Nytt himmelsengvers",
+            text: "Det har blitt lagt inn et nytt vers på himmelseng.no \nTittel: " + post.title + "\nTekst: " + post.text
+        });
+    }
+});
