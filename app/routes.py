e4ac236f25657983e38c56ae4149b9ffcef87372
Sverre Bjørke <Sverre.Bjorke@spk.no>
2018-10-18 15:25:41 +0200

    pep8

--- app/routes.py
+++ app/routes.py
@@ -16,17 +16,18 @@ class Verse(Resource):
         result = VerseEntry.query.get(verse_id)
         return (result.as_dict(), 200) if result else ({} , 404)
 
-
     def delete(self, verse_id):
         VerseEntry.query.filter_by(id=verse_id).delete()
         db.session.commit()
         return {}, 204
+
 
 class RandomVerse(Resource):
     def get(self):
         random_id = random.randrange(0, db.session.query(VerseEntry).count())
         random_verse = db.session.query(VerseEntry)[random_id]
         return random_verse.as_dict(), 200
+
 
 # VerseList
 # shows all verses and lets you POST to add new verse
@@ -43,7 +44,7 @@ class VerseList(Resource):
         return verse.as_dict(), 201
 
 
-## API routing
+# API routing
 api.add_resource(VerseList, '/api/verse')
 api.add_resource(Verse, '/api/verse/<int:verse_id>')
 api.add_resource(RandomVerse, '/api/verse/random')
