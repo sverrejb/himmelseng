4396fbf2a6ad434116911972e1985038db1084fb
Sverre Johann Bjørke <sverrejb@gmail.com>
2018-11-04 18:19:53 +0100

    allow CORS

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,12 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
