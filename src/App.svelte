ab186f37c27b9833c37576be3b53cce4a8d0f534
Sverre Johann Bjørke <sverrejb@gmail.com>
2020-01-21 22:26:17 +0100

    gogo svelte

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Verse from './Verse.svelte';
+	import verses from './verses-fixture.json';
+	export const title = "foo";
+	export const txt = verses[Math.floor(Math.random() * verses.length)].text;
+</script>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 90vw;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: darkgray;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
+<main>
+	<h1>Himmelseng</h1>
+	<Verse rverse={txt} />
+</main>
---
+++ src/Verse.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let rverse = "Placeholder text";
+</script>
+
+<style>
+    section {
+      border: 1px rgb(172, 172, 172) solid;
+    }
+</style>
+
+<section>
+  <h3>Title</h3>
+  <p>{rverse}</p>
+</section>
