ab271050393b272a75f88db81bc4db1489577fb6
Sverre Johann Bjørke <sverre.bjorke@knowit.no>
2020-01-24 17:10:37 +0100

    use custom post function

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,62 @@
 <script>
-	import Verse from './Verse.svelte';
-	import verses from './verses-fixture.json';
-	export const title = "foo";
-	export const txt = verses[Math.floor(Math.random() * verses.length)].text;
+  import Verse from "./Verse.svelte";
+  import verses from "./verses-fixture.json";
+  export const title = "foo";
+  export const txt = verses[Math.floor(Math.random() * verses.length)].text;
+
+  async function handleSubmit(event) {
+    const name = event.target.name.value;
+    const verse = event.target.verse.value;
+
+    const response = await fetch("/.netlify/functions/submit-verse", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ 'name': name, 'verse': verse })
+    });
+	const data = await response.json();
+	console.log(data)
+  }
 </script>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 90vw;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 90vw;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: darkgray;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: darkgray;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
 
 <main>
-	<h1>Himmelseng</h1>
-	<Verse rverse={txt} />
-	<form method="post" action="/.netlify/functions/submit">
-        <div>
-            <label for="name">Name: </label>
-            <input type="text" name="name" id="name" required>
-        </div>
-        <div>
-            <label for="verse">Verse: </label>
-            <input type="text" name="verse" id="verse" required>
-        </div>
-        <div>
-            <input type="submit" value="Submit!">
-        </div>
-    </form>
+  <h1>Himmelseng</h1>
+  <Verse rverse={txt} />
+  <form on:submit|preventDefault={handleSubmit}>
+    <div>
+      <label for="name">Name:</label>
+      <input type="text" name="name" id="name" required />
+    </div>
+    <div>
+      <label for="verse">Verse:</label>
+      <input type="text" name="verse" id="verse" required />
+    </div>
+    <div>
+      <input type="submit" value="Submit!" />
+    </div>
+  </form>
 </main>
