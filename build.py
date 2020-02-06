4d10ca86e4d8bb2ed3b54c3117c00c0db2fa586a
Sverre Johann Bjørke <sverre.bjorke@knowit.no>
2020-02-06 13:15:58 +0100

    added build script, updated verses

--- build.py
+++ build.py
@@ -1,1 +1,20 @@
-print("TODO")
+#!/usr/bin/env python3
+
+import os
+import json
+
+PATH = 'verses/'
+
+verses = []
+
+for filename in os.listdir(PATH):
+    with open(PATH + filename) as json_file:
+        data = json.load(json_file)
+        verses.append(data)
+
+print(verses)
+
+with open('src/verses.json', 'w') as file:
+    json.dump(verses, file)
+
+print(verses)
